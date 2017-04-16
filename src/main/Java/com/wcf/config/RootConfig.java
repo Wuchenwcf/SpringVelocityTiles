2f337ca73faf6b646f5460ca4120ed7242545664
Wuchenwcf <280298985@qq.com>
2017-04-16 15:52:33 +0800

    first commit

---
+++ src/main/Java/com/wcf/config/RootConfig.java
@@ -0,0 +1,13 @@
+package com.wcf.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.FilterType;
+@Configuration
+@ComponentScan(basePackages = { "com.wcf" }, excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig {
+
+}
