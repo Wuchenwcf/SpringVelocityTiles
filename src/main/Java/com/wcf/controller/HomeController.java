3cbf0ed7e8d5a30131b024bd9f8256a9b7e4fcb4
Wuchenwcf <280298985@qq.com>
2017-04-16 17:19:15 +0800

    second commit

--- src/main/Java/com/wcf/controller/HomeController.java
+++ src/main/Java/com/wcf/controller/HomeController.java
@@ -1,17 +1,32 @@
 package com.wcf.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.wcf.data.Clientdata;
+
 @Controller
 @RequestMapping(value="/")
 public class HomeController {
 	
+	Clientdata clientdata=new Clientdata();
+	int count=0;
+
 	@RequestMapping(method=RequestMethod.GET)
-	public String home()
+	public String home(HttpServletRequest request,Model model)
 	{
+		String ip=clientdata.getClientIp(request);
+		String mac=clientdata.getMACAddress(ip);
+		count++;
+		model.addAttribute("IP",ip);
+		model.addAttribute("MAC",mac);
+		model.addAttribute("c",count);
 		return "home";
 	}
 
